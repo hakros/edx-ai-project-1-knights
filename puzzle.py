ebb9e3a645736ef390aa1e625880556dc747677e
Hakeem <hakeemrosario@gmail.com>
2024-06-18 05:15:16 +0000

    Added knowledge bases for each puzzle

--- puzzle.py
+++ puzzle.py
@@ -12,21 +12,38 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    # Game Condition
+    Biconditional(AKnight, Not(AKnave)),
+
+    # Puzzle Conditions
+    Implication(AKnight, And(AKnave, AKnight)),
+    Implication(AKnave, Not(And(AKnave, AKnight)))
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    # Game Conditions
+    Biconditional(AKnight, Not(AKnave)),
+    Biconditional(BKnight, Not(BKnave)),
+
+    # Puzzle Conditions
+    Implication(AKnight, And(AKnave, BKnave)),
+    Implication(AKnave, Not(And(AKnave, BKnave)))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    # Game Condition
+    Biconditional(AKnight, Not(AKnave)),
+    Biconditional(BKnight, Not(BKnave)),
+
+    # Puzzle Conditions
+    Implication(Or(AKnight, BKnave), Or(And(AKnave, BKnave), And(AKnight, BKnight))),
+    Implication(Or(AKnave, BKnight), Or(And(AKnave, BKnight), And(AKnight, BKnave))),
 )
 
 # Puzzle 3
@@ -35,7 +52,19 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    # Game Condition
+    Biconditional(AKnight, Not(AKnave)),
+    Biconditional(BKnight, Not(BKnave)),
+    Biconditional(CKnight, Not(CKnave)),
+
+    Implication(BKnight, And(AKnave, CKnave)),
+    Implication(BKnave, Or(Not(AKnave), Not(CKnave))),
+
+    Implication(CKnight, AKnight),
+    Implication(CKnave, Not(AKnave)),
+
+    Implication(AKnight, CKnight),
+    Implication(AKnave, Not(CKnight)),
 )
 
 
